f551905ded4e182a4b52516d7b67acf6054141be
kevindqa <kevindqa@gmail.com>
2024-04-23 12:53:38 -0500

    cursito

---
+++ python/1/calculadora.py
@@ -0,0 +1,8 @@
+# calculadorz con operaciones basicas
+
+valor1 = 5
+valor2 = 3
+
+resultado = valor1+valor2
+
+print(resultado)
---
+++ python/1/reto.py
@@ -0,0 +1,10 @@
+#Recibe una oración como string.
+#Indica la posición de la letra con la cual empieza nuestro subtexto (la parte que vamos a “recortar”).
+#Indicar la posición de la letra con la cual termina nuestro subtexto.
+#Imprime el subtexto.
+
+str =  "hoy voy a clase en bus"
+
+subword = str [4:15]
+
+print(subword)
