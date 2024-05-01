47ca456e1344068dde8af29f5a7cf5422d1157ba
kevindqa <kevindqa@gmail.com>
2024-04-30 21:41:53 -0500

    commit de op de artimetico y de comparacion

---
+++ python/2 fundamentos py/Operadoresdecomparación.py
@@ -0,0 +1,35 @@
+# >    comprueba si es mayor que el otro valor
+print(7 > 3)
+print(3 > 7)
+print(7 > 7)
+
+# <    comprueba si es menor  que el otro valor
+print(5 < 6)
+print(6 < 5)
+print(5 < 5)
+
+# >=   comprueba si es mayor o igual que el otro valor
+print(2 >= 1)
+print(2 >= 3)
+print(2 >= 2)
+
+# <=   comprueba si es menor o igual que el otro valor
+print(1 <= 2)
+print(2 <= 1)
+print(2 <= 2)
+
+# ==   comprueba si un valor es igual a otro valor  
+
+print(6 == 6)
+print(5 == 2)
+
+# !=   comprueba si los valores son diferentes
+
+print(6 != 10)
+print(6 != 6)
+
+print("Apple" == 'Apple')
+print("Apple" == 'apple')
+print("1" == 1)
+age = 15
+print(age >= 18)
---
+++ python/2 fundamentos py/operadores aritmetico.py
@@ -0,0 +1,3 @@
+print (10/5)
+print(10%5) # se utiliza % para obtener el residuo de una operacion
+
