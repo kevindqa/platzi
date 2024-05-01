0cf978f537de37737ca97c705379948fd639b5a2
kevindqa <kevindqa@gmail.com>
2024-04-30 23:25:00 -0500

    commit de operadores

---
+++ python/2 fundamentos py/oprerador not.py
@@ -0,0 +1,20 @@
+# <<<< NOT >>>>
+
+# 	Reverse the result, returns False if the result is true
+x = 5
+
+print(not(x > 3 and x < 10))
+
+# returns False because not is used to reverse the result
+
+# example
+
+stock = int (input ("ingrese el numero de stock"))
+
+print (stock)
+
+print (not(stock>=100 and stock<=1000) )
+
+
+
+
---
+++ python/2 fundamentos py/stock.py
@@ -0,0 +1,6 @@
+stock = int (input ("ingrese el numero de stock"))
+
+print (stock)
+
+print (stock>=100 and stock<=1000) 
+
