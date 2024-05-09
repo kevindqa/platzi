67a84a58ab697a2c518888d361eb620cd9b4761c
kevindqa <kevindqa@gmail.com>
2024-05-08 23:52:24 -0500

    commit alñadiendo reto de platzi y main que es donde esta el proyecto de piedra papel o tijera

---
+++ python/2 fundamentos py/main.py
@@ -0,0 +1,35 @@
+# piedra papel o tijera 
+# para comprender el algoritmo solo nesestito saber los casos en los que yo gano
+
+jugador = input("piedra, papel o tijera  ").lower()
+import random                             # aqui importamos el modulo random 
+
+opciones = ["piedra","papel","tijera"]   # cree la variable opciones que contenia en corchetes las opciones  que el pc podia usar 
+pc = random.choice(opciones)             # aqui llamé a la funcion para que escojiera aleatoriamente una de las opciones que le habia dado anteriormente
+print(f"ecojiste = {jugador} ")
+print(f"tu oponente escoje {pc}")
+
+if jugador == pc:
+    print("empate")
+elif jugador == "piedra":                   #piedra con papel y en en else se dedujo que el computador tenia tijera porque las otras opciones ya habian sido utilizadas
+    if pc == "papel":
+        print("perdiste")
+        print("coputer gano")
+    else:
+        print("ganaste, piedra gana a tijera")
+        print("user gano")
+elif jugador == "tijera":                  #tijera con piedra
+    if pc == "piedra":
+        print("perdiste piedra gana a tijera")
+    else: 
+        print("ganaste tijera corta a papel")
+elif jugador == "papel":
+    if pc == "tijera":
+        print("perdiste tijera corta a papel")    
+    else:
+        print("ganaste papel enbuelve a la piedra")         
+else:
+    print("Entrada inválida. Por favor, elige entre piedra, papel o tijera.")   
+    
+
+
---
+++ python/2 fundamentos py/reto de numeros pares.py
@@ -0,0 +1,6 @@
+numero = int(input("escribe un numero y te dire si es par o impar  "))
+
+if numero%2==0:
+    print(f"{numero} es par ")
+else: 
+    print(f"{numero} es impar")
