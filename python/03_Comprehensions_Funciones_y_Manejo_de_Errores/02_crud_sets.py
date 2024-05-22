fe14466e5ec6ca575c95f6f719f3bb8359a0d55b
kevinquiroga <kevindqa@gmail.com>
2024-05-22 15:03:34 -0500

    agregando crud y operaciones de sets

---
+++ python/03_Comprehensions_Funciones_y_Manejo_de_Errores/02_crud_sets.py
@@ -0,0 +1,5 @@
+paises = {'colombia', 'peru', 'chile', 'ecuador'}
+print (len(paises))
+'''
+print (paises[0] )''' #no se puede acceder a un elemento dde un conjunto utilizando indices porque un conjunto es una colecion desordenada 
+print ( 'colombia' in paises ) # estaria seria la forma correcta de saber si un elemento esta en un conjunto 
