a6b08003cf2ad0ef9ebe5274fac238eeb1398036
kevinquiroga <kevindqa@gmail.com>
2024-05-23 15:27:05 -0500

    creando carpeta

---
+++ python/pa_practicar/borrar.py
@@ -0,0 +1,9 @@
+(a) = 3 
+(b) = 2 
+(c) = 1 
+
+suma= b + a +b +a +b+a+a
+
+print (suma)
+if suma in range(18,21) :
+    print('¡Tienes una gran aptitud para la ciencia de datos! Tienes las habilidades y la mentalidad necesarias para tener éxito en este campo. ')
